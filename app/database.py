5be6cf49c04f097c2a323a1cfa2f7f184390a748
M0HANRAJ-dev <mohanraj300703@gmail.com>
2026-04-20 16:14:30 +0530

    this si database.pY

--- app/database.py
+++ app/database.py
@@ -4,7 +4,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "test.db")
+
+DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:///{DB_PATH}")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
